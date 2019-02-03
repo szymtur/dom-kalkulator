37cc0cea74f0ee8d9612f9b65477afe1174feae9
szymtur <szymtur@gmail.com>
2019-02-04 00:40:49 +0100

    code_review

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ let operators = ['+', '-', '*', '/'];
 
 
 /* ZDARZENIA NA PRZYCISKU "=" */
-resultKey.addEventListener('click', function(){
+resultKey.addEventListener('click', function() {
     result();
     blockNumKeys();
     unblockOperKeys();
@@ -20,14 +20,14 @@ resultKey.addEventListener('click', function(){
 
 
 /* ZDARZENIA NA PRZYCISKU "." */
-decimalKey.addEventListener('click', function(){
+decimalKey.addEventListener('click', function() {
     blockDecimal();
 });
 
 
 /* ZDARZENIA NA PRZYCISKACH FUNKCYJNYCH */
-for(let i=0; i<operKeys.length; i++){
-    operKeys[i].addEventListener('click', function(){
+for (let i=0; i<operKeys.length; i++) {
+    operKeys[i].addEventListener('click', function() {
         unblockAllKeys();
         blockDisplayLength();
         changeFontSize();
@@ -37,8 +37,8 @@ for(let i=0; i<operKeys.length; i++){
 
 
 /* ZDARZENIA NA PRZYCISKACH NUMERYCZNYCH */
-for(let i=0; i<numKeys.length; i++){
-    numKeys[i].addEventListener('click', function(){
+for (let i=0; i<numKeys.length; i++) {
+    numKeys[i].addEventListener('click', function() {
         changeFontSize();
         blockDisplayLength();  
     });
@@ -46,14 +46,14 @@ for(let i=0; i<numKeys.length; i++){
 
 
 /* ZDARZENIA NA PRZYCISKU "CE" */
-clearKey.addEventListener('click', function(){
-     if(clearKey.className.indexOf('ac') > -1){
+clearKey.addEventListener('click', function() {
+     if (clearKey.className.indexOf('ac') > -1) {
         unblockAllKeys();
         allClear();
         changeToCE();
         changeFontSize();
     }
-    else if(clearKey.className.indexOf('ce') > -1 ){
+    else if (clearKey.className.indexOf('ce') > -1 ) {
         unblockAllKeys();
         clearEnter();
         changeFontSize();
@@ -62,9 +62,9 @@ clearKey.addEventListener('click', function(){
 
 
 /* BLOKUJE WSZYSTKIE KLAWISZE (Z WYJĄTKIEM "AC"), GDY NA EKRANIE POJAWI SIĘ ERROR */
-function blockAllKeys(display){
-    if(display == 'Error'){
-        for (let i=1; i<allKeys.length; i++){
+function blockAllKeys(display) {
+    if (display == 'Error') {
+        for (let i=1; i<allKeys.length; i++) {
             allKeys[i].disabled = true;
         }
     }
@@ -72,12 +72,12 @@ function blockAllKeys(display){
 
 
 /* BLOKUJE WSZYSTKIE KLAWISZE (OPRÓCZ "CE" I "=") PO WYPISANIU WIĘCEJ NIŻ 20 ZNAKÓW */
-function blockDisplayLength(){
-    if(display.value.length >= 20){
-        for (let i=0; i<numKeys.length; i++){
+function blockDisplayLength() {
+    if (display.value.length >= 20) {
+        for (let i=0; i<numKeys.length; i++) {
             numKeys[i].disabled = true;
         }
-        for(let i=0; i<operKeys.length; i++){
+        for (let i=0; i<operKeys.length; i++) {
             operKeys[i].disabled = true;
         }
     }
@@ -85,8 +85,8 @@ function blockDisplayLength(){
 
 
 /* BLOKUJE KLAWISZE NUMERYCZNYE PO NACIŚNIĘCIU "=" */
-function blockNumKeys(){
-    for (let i=0; i<numKeys.length; i++){
+function blockNumKeys() {
+    for (let i=0; i<numKeys.length; i++) {
         numKeys[i].disabled = true;
     }
 }
@@ -101,22 +101,22 @@ function blockDecimal() {
 
 
 /* ODBLOKOWUJE WSZYSTKIE KLAWISZE */
-function unblockAllKeys(){
-    for (let i=1; i<allKeys.length; i++){
+function unblockAllKeys() {
+    for (let i=1; i<allKeys.length; i++) {
         allKeys[i].disabled = false;
     }
 }
 
 
 /* ODBLOKOWUJE KLAWISZE FUNKCYJNE */
-function unblockOperKeys(){
-    if(display.value == 'Error'){
-        for (let i=0; i<operKeys.length; i++){
+function unblockOperKeys() {
+    if (display.value == 'Error') {
+        for (let i=0; i<operKeys.length; i++) {
             operKeys[i].disabled = true;
         }
     }
-    else{
-        for (let i=0; i<operKeys.length; i++){
+    else {
+        for (let i=0; i<operKeys.length; i++) {
             operKeys[i].disabled = false;
         }
     }
@@ -125,23 +125,23 @@ function unblockOperKeys(){
 
 /* "CE" - USUWA PO JEDNYM ZNAKU Z EKRANU */
 function clearEnter() {
-    if(display.value.length > 1){
+    if (display.value.length > 1) {
         display.value = display.value.replace(/.$/, '');
     }
-    else{
+    else {
         display.value = display.value.replace(/.$/, '0');
     }
 }
 
 
 /* "AC" - CZYŚCI CAŁY EKRAN */
-function allClear(){
+function allClear() {
     display.value = '0';
 }
 
 
 /* ZMIENIA PRZYCISK "CE" NA "AC" */
-function changeToAC(){
+function changeToAC() {
     clearKey.innerText = 'AC';
     clearKey.classList.remove('ce');
     clearKey.classList.add('ac');
@@ -149,7 +149,7 @@ function changeToAC(){
 
 
 /* ZMIENIA PRZYCISK "AC" NA "CE" */
-function changeToCE(){
+function changeToCE() {
     clearKey.innerText = 'CE';
     clearKey.classList.remove('ac') ;
     clearKey.classList.add('ce');    
@@ -157,8 +157,8 @@ function changeToCE(){
 
 
 /* ZMIENIA WIELKOŚĆ CZCIONKI EKRANU PO WPISANIU 15 ZNAKÓW */
-function changeFontSize(){
-    if(display.value.length > 15){
+function changeFontSize() {
+    if (display.value.length > 15) {
         display.style.fontSize = '30px';
     }
     else {
@@ -168,102 +168,125 @@ function changeFontSize(){
  
 
 /* FORMATUJE WYNIK, ŻEBY ZMIEŚCIŁ SIĘ NA EKRANIE */
-function resultLength(result){
-    if(result.toString().length > 15){
+function resultLength(result) {
+    if (result.toString().length > 15) {
         result = result.toPrecision(8);
     }
-    else{
+    else {
         result = result.toString();
     }
     return updateResult(result);
 }
 
 
-/* GDY WYNIK JEST LICZBĄ DZIESIĘTNĄ USUWA Z KOŃCA NIEPOTRZEBNE ZERA ORAZ "."*/ 
+/* GDY WYNIK JEST LICZBĄ DZIESIĘTNĄ USUWA Z KOŃCA NIEPOTRZEBNE ZERA ORAZ "." */ 
 function updateResult(result) {
     if (result.indexOf('.') > -1 && result.indexOf('e') == -1) {
         result = result.replace(/0+$/, '');
-        if(result.length-1 == result.lastIndexOf('.')){
+        if (result.length-1 == result.lastIndexOf('.')) {
             result = result.replace(/\.$/, '');
         }
     }
-    else{
+    else {
         result;
     }
     return result;
 }
 
 
-/* POBIERA ZNAKI Z KLAWISZY I WYPISUJE JE NA EKRAN*/
+/* POBIERA ZNAKI Z KLAWISZY I WYPISUJE JE NA EKRAN */
 function getValue(keyVal) {
     let lastChar = display.value[display.value.length-1];
 
-    //blokuje możliwość wpisania kilkukrotnie znaku " + - * / "
-    if (keyVal == '-' || keyVal == '+' || keyVal == '*' || keyVal == '/' ){
-        
-        if(operators.indexOf(lastChar) > -1){
-            display.value = display.value.replace(/.$/, '');
-        }    
-    }    
-    
-    //zamienia defaultowe "0" z ekranu na cyfrę lub dodaje do "0" znaki " + - * / . "
-    if (display.value == "0"){
+    if (display.value == "0") {
+        //dodaje do defaultowego "0" na ekranie operator " + - * / . "
         if (keyVal == '+' || keyVal == '*' || keyVal =='-' ||
-            keyVal == '/' || keyVal == '.' ){
+            keyVal == '/' || keyVal == '.' ) {
             display.value += keyVal;
         }
-        else{
+        //zamienia defaultowe "0" z ekranu na cyfrę
+        else {
             display.value = keyVal;
             }
         }
-    else{
+    //dodaje kolejne cyfry lub znaki na ekranie
+    else {
         display.value += keyVal;
     }
+
+    if (keyVal == '-' || keyVal == '+' || keyVal == '*' || keyVal == '/') {
+        //blokuje możliwość wpisania kilkukrotnie po sobie operatorów " + - * / "
+        if (operators.indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, keyVal);
+        }
+        //zamienia stojący na końcu znak "." na operator " + - * / "
+        if ( ('.').indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, keyVal); 
+        }
+    }
+
+    if (keyVal == '/' || keyVal == '*' || keyVal == '+' ) {
+        let lastTwoChars = (display.value[display.value.length-2] + display.value[display.value.length-1]);
+        
+        //blokuje użycie operatorów  " + - * / " po nawiasie "("
+        if ( ('(').indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/.$/, ''); 
+        }
+        else if(('(+').indexOf(lastTwoChars) > -1){
+            display.value = display.value.replace(/..$/, '(-'); 
+        }
+        else if(('(/').indexOf(lastTwoChars) > -1){
+            display.value = display.value.replace(/..$/, '(-');
+        }
+        else if(('(*').indexOf(lastTwoChars) > -1){
+            display.value = display.value.replace(/..$/, '(-');
+        }
+    }
     
-    if (keyVal == '0'){
+    if (keyVal == '0') {
         let twoCharsAfterLast = (display.value[display.value.length-3] + display.value[display.value.length-2]);
         
         //blokuje zero po znaku "-"
-        if (('-0').indexOf(twoCharsAfterLast) > -1){
+        if (('-0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/.$/, '');
         }
         //blokuje zero po znaku "+"
-        else if (('+0').indexOf(twoCharsAfterLast) > -1){
+        else if (('+0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/.$/, '');
         }
         //blokuje zero po znaku "*"
-        else if (('*0').indexOf(twoCharsAfterLast) > -1){
+        else if (('*0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/.$/, '');
         }
         //blokuje zero po znaku "/"
-        else if (('/0').indexOf(twoCharsAfterLast) > -1){
+        else if (('/0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/.$/, '');
         }
         //blokuje zero po nawiasie "("
-        else if (('(0').indexOf(twoCharsAfterLast) > -1){
+        else if (('(0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/.$/, '');
         }        
         //po nawiasie ")" zamienia "0" na "*0"
-        else if((')').indexOf(lastChar) > -1){
+        else if ((')').indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '*' + keyVal + ''); 
         }
     }
 
-    if (keyVal == "."){
+    if (keyVal == ".") {
         //jeżeli na końcu jest "." - blokuje dodanie kolejnych "."
-        if (('.').indexOf(lastChar) > -1){
-            display.value = display.value.replace(/.$/, '');
+        if (('.').indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, keyVal);
         }
         //po znaku " + - * / " zamienia "." na "0."
-        else if((operators).indexOf(lastChar) > -1){
+        else if ((operators).indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '0' + keyVal); 
         }
         //po nawiasie "(" zamienia "." na "0."
-        else if (('(').indexOf(lastChar) > -1){
+        else if (('(').indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '0' + keyVal); 
         }
         //po nawiasie ")" zamienia "." na "*0."
-        else if((')').indexOf(lastChar) > -1){
+        else if ((')').indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '*0' + keyVal);
         }
     }
@@ -275,65 +298,62 @@ function getValue(keyVal) {
         let twoCharsAfterLast = (display.value[display.value.length-3] + display.value[display.value.length-2]);
 
         //zamienia zero stojąca za znakiem "-" na wartość klikniętego klawisza numerycznego
-        if (('-0').indexOf(twoCharsAfterLast) > -1){
+        if (('-0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/..$/, keyVal);
         }
         //zamienia zero stojąca za znakiem "+" na wartość klikniętego klawisza numerycznego
-        else if (('+0').indexOf(twoCharsAfterLast) > -1){
+        else if (('+0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/..$/, keyVal);
         }
         //zamienia zero stojąca za znakiem "*" na wartość klikniętego klawisza numerycznego
-        else if (('*0').indexOf(twoCharsAfterLast) > -1){
+        else if (('*0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/..$/, keyVal);
         }
         //zamienia zero stojąca za znakiem "/" na wartość klikniętego klawisza numerycznego
-        else if (('/0').indexOf(twoCharsAfterLast) > -1){
+        else if (('/0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/..$/, keyVal);
         }
         //zamienia zero stojąca za nawiasem "(" na wartość klikniętego klawisza numerycznego
-        else if (('(0').indexOf(twoCharsAfterLast) > -1){
+        else if (('(0').indexOf(twoCharsAfterLast) > -1) {
             display.value = display.value.replace(/..$/, keyVal);
         }
         //dodaje znak "*" między nawias ")" i liczbę
-        else if ((')').indexOf(lastChar) > -1){
+        else if ((')').indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '*' + keyVal); 
         }
     }
     
-    if (keyVal == '(' ){
+    if (keyVal == '(' ) {
         let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         //jeśli na ekranie jest tylko "0" to zamienia je na nawias "("
-        if(display.value.length == 1){
-            if (('0').indexOf(lastChar) > -1){
+        if (display.value.length == 1) {
+            if (('0').indexOf(lastChar) > -1) {
                 display.value = display.value.replace(/.$/, keyVal); 
             }
         }
         //jeśli ostatnim znakiem jest ")" dodaje "*" przed nawias "("
-        else if ((')').indexOf(lastChar)>-1){
+        else if ((')').indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '*' + keyVal);
         }
         //dodaje znak mnożenia "*" między liczbę i nawias "("
-        else if (numbers.indexOf(lastChar) > -1){
+        else if (numbers.indexOf(lastChar) > -1) {
             display.value = display.value.replace(/.$/, '*' + keyVal); 
+        }
+        //jeśli ostatnim znakiem jest "." to zamienia go na "*("
+        else if (('.').indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, '*' + keyVal); 
         }
     }
 
-    if (keyVal == '/' || keyVal == '*' || keyVal == '+' ){
-        let lastTwoChars = (display.value[display.value.length-2] + display.value[display.value.length-1]);
-        
-        //blokuje użycie operatorów "+", "*", "/" po nawiasie "("
-        if(('(').indexOf(lastChar) > -1){
-            display.value = display.value.replace(/.$/, ''); 
+    if (keyVal == ')' ) {
+        //jeśli ostatnim znakiem jest "." to zamienia go na ")"
+        if (('.').indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, keyVal); 
         }
-        else if(('(+').indexOf(lastTwoChars) > -1){
-            display.value = display.value.replace(/..$/, '(-'); 
-        }
-        else if(('(/').indexOf(lastTwoChars) > -1){
-            display.value = display.value.replace(/..$/, '(-');
-        }
-        else if(('(*').indexOf(lastTwoChars) > -1){
-            display.value = display.value.replace(/..$/, '(-');
+        //jeżeli na końcu jest operator  " + - * / " to zamienia go na ")"
+        else if (operators.indexOf(lastChar) > -1) {
+            display.value = display.value.replace(/..$/, keyVal); 
         }
     }
 }
@@ -360,42 +380,42 @@ function checkBrackets(display) {
 
 
 /* WALIDUJE WPROWADZONY CIĄG ZNAKÓW I ZWRACA WYNIK LUB ERROR */
-function result(){
+function result() {
     
     //sprawdza czy na końcu jest znak " + - * /" i usuwa go
     let lastChar = display.value[display.value.length-1];
-    if(operators.indexOf(lastChar) > -1){
+    if (operators.indexOf(lastChar) > -1) {
         display.value = display.value.replace(/.$/, '');
     }
     
     //zamienia ")(" na ")*("
     let reg = /\)\(/;
-    for(let i=0; i<display.value.length; i++){
-        if(reg.test(display.value) == true){
+    for (let i=0; i<display.value.length; i++) {
+        if (reg.test(display.value) == true) {
             display.value = display.value.replace(/\)\(/g, ')*(');
         }    
     }
     
     //sprawdza poprawność wprowadzonego ciągu znaków i zwraca wynik lub "Error"
-    if(display.value.indexOf("(") > -1 || display.value.indexOf(")") > -1){
-        if(checkBrackets(display.value) == true){
+    if (display.value.indexOf("(") > -1 || display.value.indexOf(")") > -1) {
+        if (checkBrackets(display.value) == true) {
             let regular = /(\(+)((\-|\+)?)((\d*\.\d+)|(\d+))((\-|\+|\*|\/)((\d*\.\d+)|(\d+)))*(\)+)/;
             let error_1 = /(\d+\.\d+\.)/;               // X.X.
             let error_2 = /(\.)(\(+|\)+)/;              // .)|( 
             
-            if(regular.test(display.value) == false){
+            if (regular.test(display.value) == false) {
                     display.value = "Error";
                     blockAllKeys(display.value);
             }
-            else if(error_1.test(display.value) == true){
+            else if (error_1.test(display.value) == true) {
                     display.value = "Error";
                     blockAllKeys(display.value);
             }
-            else if(error_2.test(display.value) == true){
+            else if (error_2.test(display.value) == true) {
                     display.value = "Error";
                     blockAllKeys(display.value);
             }
-            else{
+            else {
                 let result = eval(display.value);
 
                 if (result == "Infinity" || result == "-Infinity") {
@@ -411,23 +431,23 @@ function result(){
                 }
             }
         }
-        else{
+        else {
             display.value = "Error";
             blockAllKeys(display.value); 
         }
     }   
     
-    else{
+    else {
         let error =  /(\d+\.\d+\.)/;    // X.X.
         
-        if(error.test(display.value) == true){
+        if (error.test(display.value) == true) {
            display.value = "Error";
            blockAllKeys(display.value);
         }        
-        else{
+        else {
             let result = eval(display.value);
 
-            if (result == "Infinity" || result == "-Infinity"){
+            if (result == "Infinity" || result == "-Infinity") {
                 display.value = "Error";
                 blockAllKeys(display.value);
             }
@@ -435,7 +455,7 @@ function result(){
                 display.value = "Error";
                 blockAllKeys(display.value);
             }
-            else{
+            else {
                 display.value = resultLength(result);
             }  
         }
